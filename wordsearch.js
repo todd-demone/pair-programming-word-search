a15d07596cd8e04808a080dbf8e7ed789678f232
Todd Demone <tdemone1@gmail.com>
2021-11-17 20:09:34 +0000

    complete pair programming exercise; refactor code

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// should find word if written horizontally and vertically
+const wordSearch = (letters, word) => {
+  // if letters is a empty array, then return undefined
+  if (letters.length === 0) return undefined;
+  
+  const horizontalLetters = letters.map(ls => ls.join(''));
+  const verticalLetters = transpose(letters).map(ls => ls.join(''));
 
-module.exports = wordSearch
+  return isWordPresentInArrayOfStrings(horizontalLetters, word) || isWordPresentInArrayOfStrings(verticalLetters, word) ? true : false;
+};
+
+// helper function - for finding words written vertically
+const transpose = function(matrix) {
+  const newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    const newInnerArray = [];
+    for (let j = 0; j < matrix.length; j++) {
+      newInnerArray.push(matrix[j][i]);
+    }
+    newArray.push(newInnerArray);
+  }
+  return newArray;
+};
+
+// helper function - search array of strings for the given word and return true or false, accordingly
+const isWordPresentInArrayOfStrings = function(array, word) {
+  for (const string of array) {
+    if (string.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
